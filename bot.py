d07581ff1e696b4abcff52023a477629eab6a0d8
Riw Royer <ne.free532@gmail.com>
2025-02-01 04:23:49 +0300

    добавлена возможность автоматическая  перезагрузка wireguard каждые 7 дней для сохранения статистики

--- bot.py
+++ bot.py
@@ -962,7 +962,12 @@ async def __async_restart_wireguard() -> bool:
         - Использует дефолтный ThreadPoolExecutor
         - Является internal-функцией (не предназначена для прямого вызова)
     """
-    loop = asyncio.get_running_loop()
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        
     try:
         return await loop.run_in_executor(
             None,
